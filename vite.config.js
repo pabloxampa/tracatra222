bc5666d96de3fc0d60c78c9f3dbaa6f6a2e4f887
pabloxampa <pabloxampa@gmail.com>
2026-04-13 20:41:48 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,16 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        ruleta: resolve(__dirname, 'ruleta.html'),
+        blackjack: resolve(__dirname, 'blackjack.html'),
+        slots: resolve(__dirname, 'slots.html'),
+        cartera: resolve(__dirname, 'cartera.html'),
+      },
+    },
+  },
+})
